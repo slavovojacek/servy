cc633020d15196ccfc03ae01124381016b5bf80a
Slavomir Vojacek <slavomirvojacek@gmail.com>
2017-11-05 14:57:15 +0700

    Testing

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -34,7 +34,6 @@ defmodule Servy.Handler do
     BearController.show(conv, params)
   end
 
-  # name=Baloo&type=Brown
   def route(%Conv{ method: "POST", path: "/bears" } = conv) do
     BearController.create(conv, conv.params)
   end
@@ -44,6 +43,10 @@ defmodule Servy.Handler do
       |> Path.join("about.html")
       |> File.read
       |> handle_file(conv)
+  end
+
+  def route(%Conv{ path: path } = conv) do
+    %{ conv | status: 404, resp_body: "No #{path} here!" }
   end
 
   def handle_file({:ok, content}, conv) do
@@ -58,24 +61,6 @@ defmodule Servy.Handler do
     %{ conv | status: 500, resp_body: "File error: #{reason}" }
   end
 
-  # def route(%{ method: "GET", path: "/about" } = conv) do
-  #   file =
-  #     Path.expand("../../pages", __DIR__)
-  #     |> Path.join("about.html")
-  #   case File.read(file) do
-  #     {:ok, content} ->
-  #       %{ conv | status: 200, resp_body: content }
-  #     {:error, :enoent} ->
-  #       %{ conv | status: 404, resp_body: "File not found!" }
-  #     {:error, reason} ->
-  #       %{ conv | status: 500, resp_body: "File error: #{reason}" }
-  #   end
-  # end
-
-  def route(%Conv{ path: path } = conv) do
-    %{ conv | status: 404, resp_body: "No #{path} here!" }
-  end
-
   def format_response(%Conv{} = conv) do
     """
     HTTP/1.1 #{Conv.full_status(conv)}\r
@@ -87,78 +72,3 @@ defmodule Servy.Handler do
   end
 
 end
-
-# request = """
-# GET /wildthings HTTP/1.1
-# Host: example.com
-# User-Agent: ExampleBrowser/1.0
-# Accept: */*
-
-# """
-
-# response = Servy.Handler.handle(request)
-
-# IO.puts response
-
-# request = """
-# GET /bears HTTP/1.1
-# Host: example.com
-# User-Agent: ExampleBrowser/1.0
-# Accept: */*
-
-# """
-
-# response = Servy.Handler.handle(request)
-
-# IO.puts response
-
-# request = """
-# GET /bigfoot HTTP/1.1
-# Host: example.com
-# User-Agent: ExampleBrowser/1.0
-# Accept: */*
-
-# """
-
-# response = Servy.Handler.handle(request)
-
-# IO.puts response
-
-# request = """
-# GET /bears/1 HTTP/1.1
-# Host: example.com
-# User-Agent: ExampleBrowser/1.0
-# Accept: */*
-
-# """
-
-# response = Servy.Handler.handle(request)
-
-# IO.puts response
-
-# request = """
-# GET /about HTTP/1.1
-# Host: example.com
-# User-Agent: ExampleBrowser/1.0
-# Accept: */*
-
-# """
-
-# response = Servy.Handler.handle(request)
-
-# IO.puts response
-
-# request = """
-# POST /bears HTTP/1.1
-# Host: example.com
-# User-Agent: ExampleBrowser/1.0
-# Accept: */*
-# Content-Type: application/x-www-form-urlencoded
-# Content-Length: 21
-
-# name=Baloo&type=Brown
-# """
-
-# response = Servy.Handler.handle(request)
-
-# IO.puts response
