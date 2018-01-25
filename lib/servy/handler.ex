aaef5e8d05f2623753b548cdbdb1d9fe872bc35b
Slavomir Vojacek <slavomirvojacek@gmail.com>
2018-01-25 13:24:34 +0700

    feat(Messaging): Async messaging, client, server

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -22,6 +22,14 @@ defmodule Servy.Handler do
     |> format_response
   end
 
+  def route(%Conv{method: "POST", path: "/pledges"} = conv) do
+    Servy.PledgeController.create(conv, conv.params)
+  end
+
+  def route(%Conv{method: "GET", path: "/pledges"} = conv) do
+    Servy.PledgeController.index(conv)
+  end
+
   def route(%Conv{ method: "GET", path: "/kaboom" } = conv) do
     raise "Kaboom!"
   end
@@ -63,10 +71,10 @@ defmodule Servy.Handler do
     task = Task.async(fn -> Servy.Tracker.get_location("bigfoot") end)
 
     snapshots = ["cam-1", "cam-2", "cam-3"]
-      |> Enum.map(&Task.async(fn -> VideoCam.get_snapshot(&1) end),)
+      |> Enum.map(&Task.async(fn -> VideoCam.get_snapshot(&1) end))
       |> Enum.map(&Task.await/1)
 
-    where_is_bigfoot = Task.await(pid4)
+    where_is_bigfoot = Task.await(task)
 
     %{ conv | status: 200, resp_body: inspect { snapshots, where_is_bigfoot } }
   end
