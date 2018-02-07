7374ffd030fbfde80bb212206a1c92b90b59a2d6
Slavomir Vojacek <slavomirvojacek@gmail.com>
2018-02-07 23:56:50 +0700

    chore(Misc): Finished App

--- lib/servy.ex
+++ lib/servy.ex
@@ -1,4 +1,12 @@
 defmodule Servy do
+
+  use Application
+
+  def start(_type, _args) do
+    IO.puts "Starting the Application ..."
+    Servy.Supervisor.start_link()
+  end
+
   @moduledoc """
   Documentation for Servy.
   """
